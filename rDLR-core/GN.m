7211ed3f60aa153b049b0221e4fbf43c4ab35686
hysanlam <45762587+hysanlam@users.noreply.github.com>
2024-08-02 11:42:30 +0200

    update

--- rDLR-core/GN.m
+++
@@ -1,6 +0,0 @@
-function [W,V]=GN(S,U,Theta)
-% output W, V such that X=W*V
-    [Q,R]=qr(Theta'*S,0);
-     W=S*pinv(R,10*eps(1));
-     V=Q'*U; 
-end
--- rDLR-core/calculate_pf.m
+++
@@ -1,9 +0,0 @@
-function y = calculate_pf(x0,F,t)
-    U=x0{1};
-    S=x0{2};
-    V=x0{3};
-    I=eye(size(U,1));
-    F_val=F(U*S*V',t);
-    y=(I-U*U')*(F_val*V)*V'+(U*(U'*F_val*V)*V')+U*(U'*F_val)*(I-V*V');
-
-end
